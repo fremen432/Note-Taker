8fcff9067067051af28e5527445446d01d93e5f5
Clayton Miller <cbmiller4897@gmail.com>
2021-12-28 14:31:05 -0600

    slight change

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.get('/notes', (req, res) =>{
     res.sendFile(path.join(__dirname, './public/notes.html'));
 })
 
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, './Develop/public/index.html'));
-// });
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
